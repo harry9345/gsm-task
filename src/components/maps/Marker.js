729a54293981e98ca8e43ac8951c8672c90b915a
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-08-09 11:15:07 +0300

    adding comment

--- src/components/maps/Marker.js
+++ src/components/maps/Marker.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../ui/marker.css";
 
 export default function Marker() {
+  // we also can only the "pin" also
   return (
     <>
       <div className="pin"></div>
